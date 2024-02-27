d99ddf7551064dd58d807d84523a02befaefb9d0
Alex <kokotkin.alexander@gmail.com>
2024-02-27 14:10:54 +0200

    initial commit

---
+++ Source/CombatDroneSimulator/CombatDroneSimulatorGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatDroneSimulatorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACombatDroneSimulatorGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACombatDroneSimulatorGameMode();
+};
+
+
+
